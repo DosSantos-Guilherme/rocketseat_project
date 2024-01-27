b3d09e4a49262b3f3f1888f2d7cb41e7c4c4f922
DosSantos-Guilherme <gui_santoos15@hotmail.com>
2024-01-27 03:27:31 -0300

    customizing the page

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
-  if(html.classList.contains('light')){
-    img.setAttribute('src', './assets/avatar-light.png')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/photo.png")
   } else {
-    img.setAttribute('src', './assets/avatar-light.png')
-
+    img.setAttribute("src", "./assets/photo_linkedin.jpg")
   }
 }

7f23280c8320fd03c9ac409778cae71da408f8a6
DosSantos-Guilherme <gui_santoos15@hotmail.com>
2024-01-27 12:12:08 -0300

    finished ajusting the unordered list

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/photo.png")
+    img.setAttribute("alt", "imagem de Guilherme com fundo cinza")
   } else {
     img.setAttribute("src", "./assets/photo_linkedin.jpg")
   }
